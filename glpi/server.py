73168f7878cfdbe9bfb5d9583c1215eb3bf57929
Kevin Roy <kiniou@gmail.com>
2014-07-27 14:23:35 +0200

    reorganize sources to python packages

--- glpi/server.py
+++ glpi/server.py
@@ -1,18 +1,17 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 import sys
-sys.path.append('objects')
 
 app = Flask(__name__, static_url_path = "")
- 
+
 @app.errorhandler(400)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Bad request' } ), 400)
- 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
- 
+
 @app.route('/api/v1.0/objects/<urlobject>', methods = ['GET'])
 def get_all(urlobject):
     if (module_exists(urlobject)):
